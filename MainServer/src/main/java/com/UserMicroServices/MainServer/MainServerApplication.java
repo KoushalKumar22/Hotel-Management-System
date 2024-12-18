a6117ed0906943c145a2efb16041ed17d6ec93d8
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-12-18 22:43:10 +0530

    MainServer(eureka Server)

--- MainServer/src/main/java/com/UserMicroServices/MainServer/MainServerApplication.java
+++ MainServer/src/main/java/com/UserMicroServices/MainServer/MainServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class MainServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(MainServerApplication.class, args);
 	}
