909fed0221acc95547af01ac58d4cdf0742e2f5e
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-12-18 22:42:06 +0530

    BookingService

--- Booking-Service/src/main/java/com/UserMicroServices/Booking_Service/BookingServiceApplication.java
+++ Booking-Service/src/main/java/com/UserMicroServices/Booking_Service/BookingServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BookingServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
